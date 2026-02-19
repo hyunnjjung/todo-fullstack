bb2ca7700ebc412fd08f5f7217e06178ba5f53d4
hyunnjjung <hyunnjjung@gmail.com>
2026-02-19 16:01:55 +0900

    feat: implement todo delete API

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -49,6 +49,21 @@ public class TodoService {
         return retrieve(entity.getUserId());
     }
 
+    public List<TodoEntity> delete(final TodoEntity entity){
+        validate(entity);
+
+        try {
+            repository.delete(entity);
+        }catch (Exception e){
+            log.error("error deleting entity", entity.getId(), e);
+
+            throw new RuntimeException("error deleting entity" + entity.getId());
+        }
+        return retrieve(entity.getUserId());
+    }
+
+
+
     // 🔥 validate 메서드 따로 정의
     private void validate(final TodoEntity entity){
         if(entity == null){
